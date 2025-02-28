c19f5f9d03b40ae7c098c193d03ccb8673252792
Sophia Salta <sophia.salta@student.ateneo.edu>
2025-02-28 09:17:21 +0800

    routed login to appjsx

---
+++ frontend/pages/Login.jsx
@@ -0,0 +1,13 @@
+
+{/* /frontend/pages/Login.jsx */}
+import React from 'react';
+
+export const Login = () => {
+  return (
+    <div>
+      <h1>Hephep Hooray</h1>  
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './index.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "../pages/Login";
+import './App.css';
+import './index.css';
 
 function App() {
   return (
-     <Router>
-     <div className="font-poppins">
-       <Routes>
-         <Route path="/login" element={<Login />} />
-       </Routes>
-     </div>
-   </Router>
+    <Router>
+      <div className="font-poppins">
+        <nav>
+          <a href="/login">Login</a> | 
+        </nav>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App
+
+export default App;
